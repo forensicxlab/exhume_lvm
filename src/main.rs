1fb0367923c63227396804473e1ba312b003b628
Felix Guyard <felix.guyard@forensicxlab.com>
2025-10-28 10:30:36 +0100

    Bump version

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::process;
 
 fn main() {
     let matches = Command::new("exhume_lvm")
-        .version("1.0")
+        .version("0.1.2")
         .author("ForensicXlab")
         .about("Exhumes and displays LVM information")
         .arg(
